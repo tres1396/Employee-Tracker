140bc40a2ed712ec4bfd5404edf514bc29c89ec2
Tre Scott <tres1396@gmail.com>
2021-04-16 21:38:28 -0400

    added starting function with inquirer

--- employee.js
+++ employee.js
@@ -1,20 +1,38 @@
 // Loading modules to be used in node
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-const express = require('express');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const express = require("express");
 
+// creating the connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: 'root',
-  
-    // Be sure to update with your own MySQL password!
-    password: 'test123',
-    database: 'employee_programDB',
-  });
+  host: "localhost",
 
-  
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  //
+  password: "test123",
+  database: "employee_programDB",
+});
+
+// function to start the program
+const start = function () {
+  console.log("What would you like to do?");
+  inquirer.prompt ([
+    {
+      type: "list",
+      name: "startList",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Role",
+        "Add Employees",
+        "Add Departments",
+        "Add A Role"
+      ]
+    }
+  ])
+};
